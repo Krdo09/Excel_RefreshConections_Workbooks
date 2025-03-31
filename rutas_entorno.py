39321559f52f16b5b2f29f53b3341db4c7108565
Krdo09 <agudelojulian649@gmail.com>
2025-03-31 12:07:31 -0500

    Se crea archivo .py para gestionar rutas de los archivos txt de forma independiente.

---
+++ rutas_entorno.py
@@ -0,0 +1,7 @@
+# Ingresar rutas que de los libros que se quieren actualizar en un txt, ya gregar dicha ruta al txt en el diccionario
+tipos_WorkBooks = {
+    "Hist_Vtas_PX": 
+    "S:/Z. TIC/_Qprex TIC/Scripts/Automatizaciones/Actualizacion Libros de Excel/rutas_archivos_actualizar/rutas_adaptadas/Historicos_Ventas/rutas_hist_vtas_adaptadas.txt",
+    "pruebas":
+    "S:/Z. TIC/Julian Agudelo/Archivos en construcción/Python/Actualizacion Automatica Libros Excel/rutas_archivos_actualizar/rutas_adaptadas/pruebas.txt"    
+}
